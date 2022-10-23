4c20265378f7652fb07316f44908628c244df43a
killian31 <80256683+killian31@users.noreply.github.com>
2022-10-23 16:41:09 +0200

    added every component

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(purrr)
 library(stringr)
 library(rstudioapi)
 library(DT)
+library(shinyjs)
 
 wd <- getwd()
 
@@ -29,7 +30,7 @@ ui = dashboardPage(
                 menuItem("Overview",
                          tabName = "overview",
                          icon = icon("globe")),
-              menuItem('New Contract',
+              menuItem('Upload Documents',
                        tabName = 'import',
                        icon = icon('upload')),
               menuItem("Payment",
@@ -38,18 +39,18 @@ ui = dashboardPage(
               menuItem('Sign Contract',
                        tabName = 'sign_contract',
                        icon = icon('pen')),
-              menuItem('Checking',
-                       tabName = 'checking',
+              menuItem('Receipt',
+                       tabName = 'receipt',
                        icon = icon('check'))
     
       )
     ),
-  controlbar = bs4DashControlbar(
-    skin = "light",
-    width = 350,
-     div(style = "position:relative; left:calc(10%);",
-         skinSelector())
-  ),
+  # controlbar = bs4DashControlbar(
+  #   skin = "light",
+  #   width = 350,
+  #    div(style = "position:relative; left:calc(10%);",
+  #        skinSelector())
+  # ),
   footer = dashboardFooter(
     fluidRow(
       column(11,
@@ -63,122 +64,173 @@ ui = dashboardPage(
     tabItems(
       tabItem(
         tabName = "overview",
-        tags$h4("Aide à la création des tableaux associés aux publications."),
-        tags$h5("Veuillez lire attentivement ces instructions avant de débuter :"),
+        tags$h4("Welcome to the Land Registry. Logged in as user cb027ebd54ea46301301f7703fb72f48770b5d345b563e331e5b8197fe483553"),
+        tags$h5("Please have a look at these instructions before starting."),
         div(class = "card-deck",
-            div(class = "card text-white bg-primary mb-3", style = "max-width: 18rem;",
-            div(class = "card-header", tags$h4("Import")),
+            div(class = "card text-white bg-primary mb-3", style = "max-width: 20rem;",
+            div(class = "card-header", tags$h4("New Contract")),
             div(class = "card-body",
-                div(class = "card-title", "Fichiers à importer :"),
+                div(class = "card-title", "Import your documents:"),
                 div(class = "card-text",
                     tags$ul(
-                      tags$li("Fichier de plan si nécessaire"),
-                      tags$li("Tableaux de données à formater"),
-                      tags$li("Sélection du dossier où sont stockées les tableaux")
-                    ))),
-              div(class = "card-footer", icon("exclamation-triangle", verify_fa = FALSE),
-                  " Si vous importez un fichier de plan, il est nécessaire de l'importer avant les tableaux.")),
-            div(class = "card text-white bg-secondary mb-3", style = "max-width: 18rem;",
-                div(class = "card-header", tags$h4("Paramètres d'import")),
+                      tags$li("Passport-size photograph of yourself"),
+                      tags$li("Identity proof: Aadhar card, Pan Card"),
+                      tags$li("Power of attorney"),
+                      tags$li("The latest property registers card copy"),
+                      tags$li("A copy of the municipal tax bill"),
+                      tags$li("Construction completion certificate"),
+                      tags$li("NOC (No Objection Certificate) "),
+                      tags$li("Verified Sale deed copy")
+                    )))
+              ),
+            div(class = "card text-white bg-secondary mb-3", style = "max-width: 20rem;",
+                div(class = "card-header", tags$h4("Payment")),
                 div(class = "card-body",
-                    div(class = "card-text",
-                        tags$ul(
-                          tags$li("Séparateur de colonnes"),
-                          tags$li("Séparateur décimal"),
-                          tags$li("Type de publication")
-                        ))
+                  div(class = "card-title", "Pay the fee to initiate the process."),
+                    div(class = "card-text", "The fee is required for the transfer of ownership to occur.")
                     )
                 ),
-            div(class = "card text-white bg-info mb-3", style = "max-width: 18rem;",
-                div(class = "card-header", tags$h4("Ordre des tableaux")),
+            div(class = "card text-white bg-info mb-3", style = "max-width: 20rem;",
+                div(class = "card-header", tags$h4("Fill up the contract")),
                 div(class = "card-body",
-                    div(class = "card-text",
-                        "Choix de l'ordre dans lequel vous souhaitez que vos tableaux apparaissent dans le fichier Excel formaté.")
+                    div(class = "card-title",
+                        "Fill up each fields with relevant information about you and verify the land's information")
                     )
                 ),
-            div(class = "card text-white bg-dark mb-3", style = "max-width: 18rem;",
-                div(class = "card-header", tags$h4("Formatage")),
+            div(class = "card text-white bg-success mb-3", style = "max-width: 20rem;",
+                div(class = "card-header", tags$h4("Check the transaction")),
                 div(class = "card-body",
                     div(class = "card-text",
-                        "Parcourez vos différents tableaux et définissez les différents paramètres d'apparence de chacun (par exemple taille des colonnes, unités, source, cellules à fusionner...)")
-                    ),
-                div(class = "card-footer", icon("exclamation-triangle", verify_fa = FALSE),
-                    " Si vous n'avez pas importé de plan, vous devez valider pour chaque tableau les colonnes et les unités")
-                ),
-            div(class = "card text-white bg-success mb-3", style = "max-width: 18rem;",
-                div(class = "card-header", tags$h4("Enregistrement")),
-                div(class = "card-body",
-                    div(class = "card-text",
-                        tags$ul(
-                          tags$li("Exporter le plan créé (pour réutilisation)"),
-                          tags$li("Export du fichier Excel formaté")
-                        )
+                        "Verifiy every detail of the contract before sending it."
                     )
                 )))
       ),
       tabItem(
         tabName = "import",
         div(style="display: inline-block;vertical-align:top; width: 500px;",
-            tags$p(
-              "Vous devez impérativement importer vos fichiers de données, et sélectionner le dossier qui les contient pour que le formatage fonctionne. Si vous ne choisissez pas de dossier, votre répertoire de travail actuel sera utilisé."
-            ),
-            tags$p(
-              "Si vous avez déjà créé un plan auparavant, vous pouvez l'importer afin de le modifier."
-            )),
-        div(style="display: inline-block;vertical-align:top; width: 500px;",
-            fileInput(inputId = "upload_plan",
-              label = "Importer un plan",
+            
+            fileInput(inputId = "upload_photo",
+              label = "Picture of yourself",
               multiple = FALSE,
-              accept = ".xlsx"),
-          fileInput(
-            inputId = "upload",
-            label = "Choisir les fichiers",
-            multiple = TRUE,
-            accept = c("text/csv",
-                       "text/comma-separated-values,text/plain",
-                       ".csv",
-                       ".xlsx",
-                       ".rds",
-                       ".Rds",
-                       ".parquet",
-                       ".xls",
-                       ".ods")),
-          actionBttn(inputId = "datadir", label = "Dossier de données", icon = icon("folder"), color = "royal"),
-          verbatimTextOutput(outputId = "dirpath"))
+              accept = c(".png",
+                         ".jpg")),
+            
+            fileInput(
+              inputId = "upload_id",
+              label = "Identity proof",
+              multiple = FALSE,
+              accept = c(".pdf",
+                         ".png",
+                         ".jpg")),
+            
+            fileInput(inputId = "upload_attorney",
+                      label = "Power of attorney",
+                      multiple = TRUE,
+                      accept = ".pdf"),
+            
+            fileInput(inputId = "upload_property_reg",
+                      label = "Latest property registers card copy",
+                      multiple = TRUE,
+                      accept = c(".pdf",
+                                 ".png",
+                                 ".jpg"))
+        ),
+        div(style="display: inline-block;vertical-align:top; width: 500px;",
+            fileInput(inputId = "upload_municipal_tax",
+                      label = "Municipal tax bill",
+                      multiple = TRUE,
+                      accept = ".pdf"),
+            
+            fileInput(inputId = "upload_construction",
+                      label = "Construction completion certificate",
+                      multiple = TRUE,
+                      accept = ".pdf"),
+            
+            fileInput(inputId = "upload_no_objection",
+                      label = "No Objection Certificate",
+                      multiple = TRUE,
+                      accept = ".pdf"),
+            
+            fileInput(inputId = "upload_deed",
+                      label = "Verified Sale deed copy",
+                      multiple = TRUE,
+                      accept = ".pdf")
+          ),
+        div(style = "position:relative; left:calc(30%);",
+            actionBttn(inputId = "validate_docs",
+                     label = "Send documents",
+                     icon = icon("check"),
+                     color = "success")
+        )
       ),
       tabItem(tabName = "payment",
-              div(style="display: inline-block;vertical-align:top; width: 500px;",
-                  radioButtons("sep", "Separateur",
-                     choices = c(Virgule = ",",
-                                 "Point Virgule" = ";",
-                                 Tab = "\t"),
-                     selected = ","),
-              tags$hr(),
-              radioButtons("virg", "Séparateur décimal",
-                           choices = c(Virgule = ",",
-                                       Point = "."),
-                           selected = ",")),
-              div(style="display: inline-block;vertical-align:top; width: 500px;",
-                  prettyRadioButtons(inputId = "format",
-                                     label = "Type de publication",
-                                     choices = c("Chiffres et données" = "chiffres_et_donnees",
-                                                 Primeur = "primeur"),
-                                     selected = "chiffres_et_donnees"),
-                  checkboxInput(
-                    inputId = "largeur_max",
-                    label = "Utiliser la règle de diffusion de largeur maximale des tableaux (actuellement 83)",
-                    value = TRUE
-                  )
-                )),
+              p("The button below will redirect you to the payment page."),
+              actionBttn(inputId = "pay_btn",
+                         label = "Procced to payment",
+                         icon = icon("money"),
+                         color = "success")
+              ),
       tabItem(tabName = "sign_contract",
-              p("Sélectionnez tous les fichiers dans l'ordre que vous souhaitez pour le modifier :"),
-              DTOutput("files_order"),
-              verbatimTextOutput("liste_ordre"),
-              actionBttn("validate_order", "Valider", color = "success",
-                         icon = icon("check"))),
-      tabItem(tabName = "checking",
-              downloadBttn("exporter", "Exporter le plan"),
-              downloadBttn("enreg", "Enregistrer les tableaux formatés"))
+              div(style="display: inline-block;vertical-align:top; width: 1000px;",
+                  tags$h1("Your contract"),
+                  p("This contract was initiated by seller bef3cde7136ba829665ebe1b2a36484375d7c8515558655b9343fd8ee1842f10"),
+                  p(""),
+                  useShinyjs(),
+                  disabled(textInput(inputId = "land_id",
+                            label = "Land ID",
+                            value = "83c89702f4d913cd904ec99a4531a5a81d7ba2a12ce68e205a6fef2526511aef",
+                            width = "1000px")),
+                  disabled(textInput(inputId = "land_location",
+                                     label = "Land location",
+                                     value = "2, Air India Rd, Kismat Nagar, Kurla West, Mumbai, Maharashtra 400070, India",
+                                     width = "1000px")),
+                  disabled(textInput(inputId = "land_value",
+                                     label = "Transaction value",
+                                     value = "Rs. 40000000",
+                                     width = "1000px")),
+                  disabled(textInput(inputId = "seller_name",
+                                     label = "Seller's name",
+                                     value = "Droupadi Murmu",
+                                     width = "1000px")),
+                  tags$hr(),
+                  textInput(inputId = "name_input",
+                            label = "Full name",
+                            width = "1000px"),
+                  textInput(inputId = "aadhaar_input",
+                            label = "Aadhaar number",
+                            width = "1000px"),
+                  dateInput(inputId = "birth_input",
+                            label = "Birth date",
+                            format = "dd-mm-yyyy",
+                            width = "1000px"), 
+                  textInput(inputId = "address_input",
+                            label = "Address",
+                            width = "1000px"),
+                  textInput(inputId = "bankId_input",
+                            label = "Bank ID",
+                            width = "1000px"),
+                  tags$hr(),
+                  selectInput(inputId = "transaction_type",
+                              label = "Nature of transaction",
+                              choices = c('Whole land',
+                                          'Split land')),
+                  selectInput(inputId = "land_type",
+                              label = "Type of land",
+                              choices = c('Residential',
+                                          'Agricultural',
+                                          'Industrial',
+                                          'Work area')),
+                  dateInput(inputId = "deadline_date",
+                            label = "Deadline date",
+                            format = "dd-mm-yyyy",
+                            width = "1000px"),
+                  actionBttn(inputId = "sign_contract_btn",
+                             label = "Sign contract digitally",
+                             icon = icon("signature"),
+                             color = "success")
+                 )
+              ),
+      tabItem(tabName = "receipt")
     )
   )
 )
@@ -189,7 +241,7 @@ server = function(input, output, session) {
   
   # next and previous buttons
   global <- reactiveValues(tab_id = "")
-  tab_id <- c('overview', 'import','payment','sign_contract','checking')
+  tab_id <- c('overview', 'import','payment','sign_contract','receipt')
   
   Current <- reactiveValues(
     Tab = "overview"
@@ -226,9 +278,21 @@ server = function(input, output, session) {
     stopApp()
   })
   
+  observeEvent(input$validate_docs, {
+    is_error <- FALSE
+    if is.null(input$upload_photo){
+      is_error <- TRUE
+      shinyalert(title = "A document is missing", text = "The photo has not been uploaded."; type = "error")
+    }
+    if !is_error {
+      shinyalert(title = "All documents have been sent", type = "success")
+    }
+
+  })
   
 }
 
 shinyApp(ui = ui,
         server = server,
         options = list(launch.browser = TRUE))
+
