7ba231abad32400bb67dfa953c657164aab3f09b
killian31 <80256683+killian31@users.noreply.github.com>
2022-10-23 19:26:47 +0200

    updated order of tabs

--- app.R
+++ app.R
@@ -30,12 +30,12 @@ ui = dashboardPage(
                 menuItem("Overview",
                          tabName = "overview",
                          icon = icon("globe")),
+                menuItem("Payment",
+                         tabName = 'payment',
+                         icon = icon('money')),
               menuItem('Upload Documents',
                        tabName = 'import',
                        icon = icon('upload')),
-              menuItem("Payment",
-                       tabName = 'payment',
-                       icon = icon('money')),
               menuItem('Sign Contract',
                        tabName = 'sign_contract',
                        icon = icon('pen')),
@@ -67,15 +67,20 @@ ui = dashboardPage(
         tags$h4("Welcome to the Land Registry. Logged in as user cb027ebd54ea46301301f7703fb72f48770b5d345b563e331e5b8197fe483553"),
         tags$h5("Please have a look at these instructions before starting."),
         div(class = "card-deck",
+            div(class = "card text-white bg-secondary mb-3", style = "max-width: 20rem;",
+                div(class = "card-header", tags$h4("Payment")),
+                div(class = "card-body",
+                    div(class = "card-title", "Pay the fee to initiate the process."),
+                    div(class = "card-text", "The fee is required for the transfer of ownership to occur.")
+                )
+            ),
             div(class = "card text-white bg-primary mb-3", style = "max-width: 20rem;",
             div(class = "card-header", tags$h4("Upload Documents")),
             div(class = "card-body",
                 div(class = "card-title", "Import your documents:"),
                 div(class = "card-text",
                     tags$ul(
-                      tags$li("Passport-size photograph of yourself"),
                       tags$li("Identity proof: Aadhar card, Pan Card"),
-                      tags$li("Power of attorney"),
                       tags$li("The latest property registers card copy"),
                       tags$li("A copy of the municipal tax bill"),
                       tags$li("Construction completion certificate"),
@@ -83,13 +88,6 @@ ui = dashboardPage(
                       tags$li("Verified Sale deed copy")
                     )))
               ),
-            div(class = "card text-white bg-secondary mb-3", style = "max-width: 20rem;",
-                div(class = "card-header", tags$h4("Payment")),
-                div(class = "card-body",
-                  div(class = "card-title", "Pay the fee to initiate the process."),
-                    div(class = "card-text", "The fee is required for the transfer of ownership to occur.")
-                    )
-                ),
             div(class = "card text-white bg-info mb-3", style = "max-width: 20rem;",
                 div(class = "card-header", tags$h4("Fill up the contract")),
                 div(class = "card-body",
@@ -105,15 +103,16 @@ ui = dashboardPage(
                     )
                 )))
       ),
+      tabItem(tabName = "payment",
+              p("The button below will redirect you to the payment page."),
+              actionBttn(inputId = "pay_btn",
+                         label = "Procced to payment",
+                         icon = icon("money"),
+                         color = "success")
+      ),
       tabItem(
         tabName = "import",
         div(style="display: inline-block;vertical-align:top; width: 500px;",
-            
-            fileInput(inputId = "upload_photo",
-              label = "Picture of yourself",
-              multiple = FALSE,
-              accept = c(".png",
-                         ".jpg")),
             
             fileInput(
               inputId = "upload_id",
@@ -123,26 +122,21 @@ ui = dashboardPage(
                          ".png",
                          ".jpg")),
             
-            fileInput(inputId = "upload_attorney",
-                      label = "Power of attorney",
-                      multiple = TRUE,
-                      accept = ".pdf"),
-            
             fileInput(inputId = "upload_property_reg",
                       label = "Latest property registers card copy",
                       multiple = TRUE,
                       accept = c(".pdf",
                                  ".png",
-                                 ".jpg"))
+                                 ".jpg")),
+            
+            fileInput(inputId = "upload_construction",
+                      label = "Construction completion certificate",
+                      multiple = TRUE,
+                      accept = ".pdf")
         ),
         div(style="display: inline-block;vertical-align:top; width: 500px;",
             fileInput(inputId = "upload_municipal_tax",
                       label = "Municipal tax bill",
-                      multiple = TRUE,
-                      accept = ".pdf"),
-            
-            fileInput(inputId = "upload_construction",
-                      label = "Construction completion certificate",
                       multiple = TRUE,
                       accept = ".pdf"),
             
@@ -163,13 +157,6 @@ ui = dashboardPage(
                      color = "success")
         )
       ),
-      tabItem(tabName = "payment",
-              p("The button below will redirect you to the payment page."),
-              actionBttn(inputId = "pay_btn",
-                         label = "Procced to payment",
-                         icon = icon("money"),
-                         color = "success")
-              ),
       tabItem(tabName = "sign_contract",
               div(style="display: inline-block;vertical-align:top; width: 1000px;",
                   tags$h1("Your contract"),
@@ -214,12 +201,12 @@ ui = dashboardPage(
                               label = "Nature of transaction",
                               choices = c('Whole land',
                                           'Split land'))),
-                  selectInput(inputId = "land_type",
+                  disabled(selectInput(inputId = "land_type",
                               label = "Type of land",
                               choices = c('Residential',
                                           'Agricultural',
                                           'Industrial',
-                                          'Work area')),
+                                          'Work area'))),
                   disabled(dateInput(inputId = "deadline_date",
                             label = "Deadline date",
                             format = "dd-mm-yyyy",
@@ -244,7 +231,7 @@ server = function(input, output, session) {
   
   # next and previous buttons
   global <- reactiveValues(tab_id = "")
-  tab_id <- c('overview', 'import','payment','sign_contract','receipt')
+  tab_id <- c('overview','payment', 'import','sign_contract','receipt')
   
   Current <- reactiveValues(
     Tab = "overview"
@@ -283,22 +270,10 @@ server = function(input, output, session) {
   
   observeEvent(input$validate_docs, {
     is_error <- FALSE
-    if (is.null(input$upload_photo) & !is_error){
-      is_error <- TRUE
-      shinyalert(title = "A document is missing",
-                 text = "The photo has not been uploaded.",
-                 type = "error")
-    }
     if (is.null(input$upload_id) & !is_error){
       is_error <- TRUE
       shinyalert(title = "A document is missing",
                  text = "The ID has not been uploaded.",
-                 type = "error")
-    }
-    if (is.null(input$upload_attorney) & !is_error){
-      is_error <- TRUE
-      shinyalert(title = "A document is missing",
-                 text = "The power of attorney has not been uploaded.",
                  type = "error")
     }
     if (is.null(input$upload_property_reg) & !is_error){
@@ -433,7 +408,6 @@ server = function(input, output, session) {
     toggleState(id = "birth_input")
     toggleState(id = "address_input")
     toggleState(id = "bankId_input")
-    toggleState(id = "land_type")
   })
   
   output$download_receipt <- downloadHandler(
